9ee7986c0c9858abac70a402cf0fc5151d2a861c
EstherRam <127980886+EstherRam@users.noreply.github.com>
2025-08-18 09:13:10 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,16 @@
  * - React Docs (Rendering Elements): https://react.dev/learn
  * - MDN DOM reference: https://developer.mozilla.org/en-US/docs/Web/API/Document
  */
+// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './index.css'   // Tailwind layers
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
+
 
