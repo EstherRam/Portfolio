b2ec787c11503301a87863cbd233893f72573c0d
EstherRam <127980886+EstherRam@users.noreply.github.com>
2025-08-18 08:31:57 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,11 @@
 // References:
 // - Vite config: https://vitejs.dev/config/
 // - Vite static deploy: https://vitejs.dev/guide/static-deploy.html
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// References:
-// – Vite config: https://vitejs.dev/config/
-// – Vite + Pages: https://vitejs.dev/guide/static-deploy.html
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/'
-})
+  base: "/Portfolio/" // must match repo name (case-sensitive)
+});
 
