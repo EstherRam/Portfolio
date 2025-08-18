98cc17e1899c8ed916714daf90d4621b17ca68b0
EstherRam <127980886+EstherRam@users.noreply.github.com>
2025-08-18 08:22:45 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,10 @@
  * - React Docs (Rendering Elements): https://react.dev/learn
  * - MDN DOM reference: https://developer.mozilla.org/en-US/docs/Web/API/Document
  */
-
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './index.css' 
 
 createRoot(document.getElementById('root')).render(<App />)
+
