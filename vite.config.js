48d145cd9d27ae8c2830f1a00fb6aa0f80064734
EstherRam <127980886+EstherRam@users.noreply.github.com>
2025-08-18 12:37:02 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/', // <-- repo name (case-sensitive)
+  base: '/Portfolio/', 
 })
 
 
