8fbef84dd8619fa3222acf6df59a5f630cf82757
EstherRam <127980886+EstherRam@users.noreply.github.com>
2025-08-18 07:47:59 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // References:
-// - Vite config docs: https://vitejs.dev/config/
-// - GitHub Pages setup with Vite: https://vitejs.dev/guide/static-deploy.html
-
+// – Vite config: https://vitejs.dev/config/
+// – Vite + Pages: https://vitejs.dev/guide/static-deploy.html
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/'
+  base: '/Portfolio/', //repo name
 })
+
