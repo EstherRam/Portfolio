94f1e72dd57a5735c9b57a782c43a86e843e42b6
EstherRam <127980886+EstherRam@users.noreply.github.com>
2025-08-18 10:10:56 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -465,16 +465,16 @@ export default function App() {
             <p className="mt-3 text-slate-600 text-sm">
               Email:{" "}
               <a
-                href="mailto:estherramcharan@example.com"
+                href="mailto:ramchaes@sheridancollege.ca"
                 className="text-sky-700 hover:underline"
               >
-                estherramcharan@example.com
+                ramchaes@sheridancollege.ca
               </a>
             </p>
             <p className="text-slate-600 text-sm">
-              Portfolio:{" "}
+              Linkedin:{" "}
               <a
-                href="https://ramchaes.myportfolio.com/work"
+                href="www.linkedin.com/in/estram"
                 className="text-sky-700 hover:underline"
               >
                 ramchaes.myportfolio.com/work
