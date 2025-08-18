05a9f5faea00f9e4d6e33f58dbb4f130dd68a0a1
EstherRam <127980886+EstherRam@users.noreply.github.com>
2025-08-18 08:17:00 -0400

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+/**
+ * PostCSS pipeline for Tailwind
+ * References:
+ * - PostCSS: https://postcss.org/
+ * - Tailwind + PostCSS setup: https://tailwindcss.com/docs/installation
+ */
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
